28b93a090c1cfe8051985cf4a2dabe10792db4db
mayor167 <adenijimayokun@gmail.com>
2023-09-04 19:41:57 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle -function Checks if a singly-linked list contains a cycle.
+ * @list: A pointer singly-linked list.
+ *
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *tortle, *haire;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	tortle = list->next;
+	hare = list->next->next;
+
+	while (tortle && haire && haire->next)
+	{
+		if (tortle == haire)
+			return (1);
+
+		tortle = tortle->next;
+		haire = haire->next->next;
+	}
+
+	return (0);
+}
