4f4262bc2af5750180b68083a9a90aa02fffa17c
mayor167 <adenijimayokun@gmail.com>
2023-09-12 02:46:51 -0700

    done

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    my_new_string = ""
+    for char in my_string:
+        if char not in ('c', 'C'):
+            my_new_string += char
+
+    return (my_new_string)
