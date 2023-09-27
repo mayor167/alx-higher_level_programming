ca247b114159a999106d502d5ff48099b2e39120
mayor167 <adenijimayokun@gmail.com>
2023-09-26 23:26:51 -0700

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""code here..."""
+
+
+class Sqaure:
+    """initialise here and privatise the attribute size here"""
+
+    def __init__(self, size):
+
+        self.__size = size
