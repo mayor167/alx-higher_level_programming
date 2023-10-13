677d1e56951ac45979cb4ec4cf7a601eae190334
mayor167 <adenijimayokun@gmail.com>
2023-10-13 04:38:20 -0700

    done

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
-"""Log parsing script."""
 import sys
 
-total_sz = 0
-codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-         '403': 0, '404': 0, '405': 0, '500': 0}
-iterat = 0
+
+def print_info():
+    print('File size: {:d}'.format(file_size))
+
+    for scode, code_times in sorted(status_codes.items()):
+        if code_times > 0:
+            print('{}: {:d}'.format(scode, code_times))
 
 
-def print_stats():
-    """Function that prints resume of stats."""
-    print("File size: {}".format(total_sz))
-    for list_items, m in sorted(codes.items()):
-        if m is not 0:
-            print("{}: {}".format(list_items, m))
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
 
+lc = 0
+file_size = 0
 
 try:
     for line in sys.stdin:
-        line = line.split()
-        if len(line) >= 2:
-            temp = iterat
-            if line[-2] in codes:
-                codes[line[-2]] += 1
-                iterat += 1
-            try:
-                total_sz += int(line[-1])
-                if temp == iterat:
-                    iterat += 1
-            except ValueError:
-                if temp == iterat:
-                    continue
+        if lc != 0 and lc % 10 == 0:
+            print_info()
 
-        if iterat % 10 == 0:
-            print_stats()
+        pieces = line.split()
 
-    print_stats()
+        try:
+            status = int(pieces[-2])
 
+            if str(status) in status_codes.keys():
+                status_codes[str(status)] += 1
+        except ValueError:
+            pass
+
+        try:
+            file_size += int(pieces[-1])
+        except ValueError:
+            pass
+
+        lc += 1
+
+    print_info()
 except KeyboardInterrupt:
-    print_stats()
+    print_info()
+    raise
