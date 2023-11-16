d1b7c15d9d8e9d198cde0541a8810962b97a5de6
mayor167 <adenijimayokun@gmail.com>
2023-11-15 18:42:26 -0800

    done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Creates the user user_0d_1 with all privileges.
+SELECT * FROM mysql.user WHERE User = 'user_0d_1';
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'%'
     IDENTIFIED BY 'user_0d_1_pwd';
