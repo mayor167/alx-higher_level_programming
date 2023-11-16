7fa6053d8e0509779af0215044694c2f62ca544c
mayor167 <adenijimayokun@gmail.com>
2023-11-15 18:50:57 -0800

    done

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,12 @@
--- Makes the user user_0d_1 with all privileges.
+-- Creates the user user_0d_1 with all privileges.
 CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
+    'user_0d_1'@'localhost'
+    IF NOT EXISTS
     IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost'
    IDENTIFIED BY 'user_0d_1_pwd';
+
 FLUSH PRIVILEGES;
